db5698209fa7b04fccdc4c7393e1b85f07afd2fd
Pavel Nezhencev <pavel.nezhencev@creuna.com>
2014-07-09 16:49:11 +0300

    Refactorings and name changes

---
+++ MockConfigs/ConfigMocker/ConfigMocker.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web.Configuration;
+using System.Web.Hosting;
+using System.Xml.Linq;
+
+namespace ConfigMocker
+{
+    public class ConfigMocker : IConfigMocker
+    {
+        public void Mock()
+        {
+            MockConnectionStrings();
+            MockAppSettings();
+        }
+
+        public void MockConnectionStrings()
+        {
+            var alternativeConfig = GetAlternativeConfigFor("connectionStrings");
+
+            if (alternativeConfig == null)
+            {
+                return;
+            }
+
+            var newConnStrings = alternativeConfig.Descendants("connectionStrings").Elements("add").ToList();
+            var connString = WebConfigurationManager.ConnectionStrings;
+
+            foreach (ConnectionStringSettings cs in connString)
+            {
+                var readOnly = typeof (ConfigurationElement).GetField("_bReadOnly",BindingFlags.Instance | BindingFlags.NonPublic);
+                readOnly.SetValue(cs, false);
+                var csReplacement = newConnStrings.FirstOrDefault(x => x.Attribute("name").Value == cs.Name);
+                if (csReplacement != null)
+                {
+                    cs.ConnectionString = csReplacement.Attribute("connectionString").Value;
+                }
+            }
+        }
+
+        public void MockAppSettings()
+        {
+            var alternativeConfig = GetAlternativeConfigFor("appSettings");
+
+            if (alternativeConfig == null)
+            {
+                return;
+            }
+
+            var newSettings = alternativeConfig.Descendants("appSettings").Elements("add").ToList();
+            var appSettings = WebConfigurationManager.AppSettings;
+
+            for (var i = 0; i < appSettings.Count; i++)
+            {
+                var key = appSettings.GetKey(i);
+
+                var settingReplacement = newSettings.FirstOrDefault(x => x.Attribute("key").Value == key);
+                if (settingReplacement != null)
+                {
+                    appSettings[key] = settingReplacement.Attribute("value").Value;
+                }
+            }
+
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+
+        private XDocument GetAlternativeConfigFor(string sectionName)
+        {
+            var path = GetPathToAlternativeConfigs("appSettings");
+
+            return File.Exists(path) ? XDocument.Load(path) : null;
+        }
+
+        private string GetPathToAlternativeConfigs(string sectionName)
+        {
+            var section = WebConfigurationManager.OpenWebConfiguration(null).GetSection(sectionName);
+            var source = section.SectionInformation.ConfigSource;
+
+            if (string.IsNullOrEmpty(source))
+            {
+                source = "Web.config";
+            }
+
+            return HostingEnvironment.MapPath("~/" + Path.ChangeExtension(source, Environment.MachineName + ".config"));
+        }
+    }
+}
---
+++ MockConfigs/ConfigMocker/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+// General info about the product
+[assembly: AssemblyTitle("ConfigMocker")]
+[assembly: AssemblyDescription("Allow having machine specific configs.")]
+[assembly: AssemblyCompany("Dmitriy Martyscenko")]
+[assembly: AssemblyProduct("ConfigMocker")]
+[assembly: AssemblyCopyright("Copyright © Dmitriy Martyscenko 2014")]
+
+// Product version
+[assembly: AssemblyVersion("0.1")]
+[assembly: AssemblyFileVersion("0.1")]
